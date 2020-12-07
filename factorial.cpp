4651375b4497cb0821d4ed1c2362b2558d23ea6e
Biki-das <72331432+Biki-das@users.noreply.github.com>
2020-12-07 19:29:23 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int factorial(int n);
+
+using namespace std;
+
+int main(){
+
+
+int n;
+cout<<"enter your number "<<endl;
+cin >> n;
+
+cout<<"the factoria for this value is<<factorial(n)<<endl;
+
+
+
+
+
+
+return 0;
+}
+int factorial(int n){
+
+if n > 1 {
+
+return factorial * n(n - 1);
+
+}
+ else
+ return 1;
+
+
+
+}
